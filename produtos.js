5050be51347cceac2f25f39438e0dae4f7d0a7d1
isabel1971 <52551178+isabel1971@users.noreply.github.com>
2024-03-19 21:29:23 -0300

    corrigir produtos.js

--- produtos.js
+++ produtos.js
@@ -3,31 +3,27 @@ async function criarProdutos(cd) {
 
     (async () => {
     
-    let NomeProduto = document.getElementById("Nome do Produto").value;
-    let quantidade = document.getElementById("Quantidade").value;
-    let perecivel = document.getElementById("Perecível").value;
-    let dataValidade = document.getElementById("Data de Validade").value;
+        let NomeProduto = document.getElementById("Nome do Produto").value;
+        let quantidade = document.getElementById("Quantidade").value;
+        let perecivel = document.getElementById("Perecível").value;
+        let dataValidade = document.getElementById("Data de Validade").value;
         
-    const myNewObject = new Parse.Object('Produtos');
+        const myNewObject = new Parse.Object('Produtos');
 
-    myNewObject.set('Nome do Produto', NomeProdut);
-    myNewObject.set('Quantidade', quantidade);
-    myNewObject.set('Perecível', perecivel);
-    myNewObject.set('Data de Validade', dataValidade);
+        myNewObject.set('NomeProduto', NomeProduto); 
+        myNewObject.set('quantidade', quantidade); 
+        myNewObject.set('perecivel', perecivel); 
+        myNewObject.set('dataValidade', dataValidade); 
     
-    try {
-        const result = await myNewObject.save();
+        try {
+            const result = await myNewObject.save();
  
-        console.log('Produtos created', result);
-        alert(
-            "Produto cadastrado com sucesso!"
-        );
-    } catch (error) {
-        console.error('Error while creating Doador: ', error);
-        alert(
-            "Erro. Não foi possível cadastrar o Produto."
-        )
-    }
+            console.log('Produtos created', result);
+            alert("Produto cadastrado com sucesso!");
+        } catch (error) {
+            console.error('Error while creating Produtos: ', error); 
+            alert("Erro. Não foi possível cadastrar o Produto.");
+        }
     })();
 }
 
@@ -40,31 +36,29 @@ async function lerDoador(ld) {
         try {
             const results = await query.find();
             for (const object of results) {
-                // Access the Parse Object attributes using the .GET method
-                const NomeProduto = object.get('NomeProduto')
-                const quantidade = object.get('quantidade')
-                const perecivel = object.get('perecivel')
-                const dataValidade = object.get('dataValidade')
+                
+                const NomeProduto = object.get('NomeProduto'); 
+                const quantidade = object.get('quantidade'); 
+                const perecivel = object.get('perecivel'); 
+                const dataValidade = object.get('dataValidade'); 
                 console.log(NomeProduto);
                 console.log(quantidade);
                 console.log(perecivel);
                 console.log(dataValidade);
-
             }
-
-         } catch (error) {
+        } catch (error) {
             console.error('Error while fetching Produtos', error);
         }
     })();
 }
 
-    async function atualizarProdutos(ad) {
+async function atualizarProdutos(ad) {
     ad.preventDefault();
 
     (async () => {
+        const Produtos = Parse.Object.extend('Produtos'); 
         const query = new Parse.Query(Produtos);
         try {
-            
             const object = await query.get('xKue915KBG');
             object.set('NomeProduto', 'A string');
             object.set('quantidade', 1);
@@ -72,39 +66,35 @@ async function lerDoador(ld) {
             object.set('dataValidade', new Date());
             try {
                 const response = await object.save();
-            
                 console.log(response.get('NomeProduto'));
                 console.log(response.get('quantidade'));
                 console.log(response.get('perecivel'));
                 console.log(response.get('dataValidade'));
-                console.log(response.get('doacaoEspecie'));
-                console.log(response.get('valorDoacao'));
                 console.log('Produtos updated', response);
-              } catch (error) {
+            } catch (error) {
                 console.error('Error while updating Produtos', error);
-                }
-              } catch (error) {
-                console.error('Error while retrieving object Produtos', error);
-              }
+            }
+        } catch (error) {
+            console.error('Error while retrieving object Produtos', error);
+        }
     })();
 }
 
-    async function excluirProdutos(ed) {
+async function excluirProdutos(ed) {
     ed.preventDefault();
 
     (async () => {
-    const query = new Parse.Query('Produtos');
-    try {
- 
-        const object = await query.get('xKue915KBG');
+        const query = new Parse.Query('Produtos');
         try {
-          const response = await object.destroy();
-          console.log('Deleted ParseObject', response);
+            const object = await query.get('xKue915KBG');
+            try {
+                const response = await object.destroy();
+                console.log('Deleted ParseObject', response);
+            } catch (error) {
+                console.error('Error while deleting ParseObject', error);
+            }
         } catch (error) {
-          console.error('Error while deleting ParseObject', error);
+            console.error('Error while retrieving ParseObject', error);
         }
-      } catch (error) {
-        console.error('Error while retrieving ParseObject', error);
-      }
     })();
 }
